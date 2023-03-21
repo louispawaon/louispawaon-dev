0d543078efd0b275d0ca3e5a7034db860c7b0dac
louispawaon <lpawaon@gmail.com>
2023-03-21 10:24:12 +0800

    init compontents

---
+++ src/app/compontents/ComponentIndex.tsx
@@ -0,0 +1,6 @@
+export {default as Creations} from './Creations/Creations';
+export {default as Experience} from './Experience/Experience';
+export {default as Footer} from './Footer/Footer';
+export {default as Header} from './Header/Header';
+export {default as Navbar} from './NavBar/Navbar';
+export {default as Technologies} from './Technologies/Technologies';
