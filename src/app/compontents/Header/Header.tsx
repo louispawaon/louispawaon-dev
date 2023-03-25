a2ef06b9730e597d68e34f7bc373f2ed05717d90
louispawaon <lpawaon@gmail.com>
2023-03-25 16:15:51 +0800

    update header - animated wave added

--- src/app/compontents/Header/Header.tsx
+++ src/app/compontents/Header/Header.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import Lottie from "lottie-react";
+import handWave from '../../../../public/wave.json';
 
 export default function Header(){
     return(
         <div className="space-y-5 py-20 min-h-[100vh]">
-            <h1 className="sm:leading-tight lg:leading-tight sm:text-[70px] md:text-[80px] lg:text-[85px] sm:text-4xl font-semibold">hi 👋, migs here.</h1>
+            <div>
+                <h1 className="sm:leading-tight lg:leading-tight sm:text-[70px] md:text-[80px] lg:text-[85px] sm:text-4xl font-semibold">hi <Lottie animationData={handWave} className="w-[100px] inline-block"/>,  <span className="font-bold">migs</span> here. </h1>
+            </div>
             <h1 className="md:text-4xl sm:text-lg mb-5">bringing my imagination to life.</h1>
             <p className="flex md:text-[22px] sm:text-[15px] text-justify max-w-[60%]">I'm a javascript developer based in The Philippines. 
             I have great interest in full-stack development and machine learning.</p>
