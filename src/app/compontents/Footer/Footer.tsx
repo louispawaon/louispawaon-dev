e4a9e783dd8ce5865d9ca8804a07c8729817fc7b
louispawaon <lpawaon@gmail.com>
2023-03-29 15:17:36 +0800

    update footer

--- src/app/compontents/Footer/Footer.tsx
+++ src/app/compontents/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <div className=" p-6 text-center dark:bg-neutral-700">
+        <div className=" p-6 text-center">
             <span>Made With <span>💗</span> by Louis Miguel Pawaon</span>
         </div>
     )
