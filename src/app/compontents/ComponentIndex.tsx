e0802c9b6a129caf1cbb6a87bdd4ac38c3664412
louispawaon <lpawaon@gmail.com>
2024-02-16 14:26:09 +0800

    feat: banner for announement

--- src/app/compontents/ComponentIndex.tsx
+++ src/app/compontents/ComponentIndex.tsx
@@ -5,3 +5,4 @@ export {default as Footer} from './Footer/Footer';
 export {default as Header} from './Header/Header';
 export {default as Sidebar} from './SideBar/Sidebar';
 export {default as Technologies} from './Experience/Technologies';
+export {default as Banner} from './Header/Banner';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react"
-import {Experience,Footer,Header,Sidebar} from "@/app/compontents/ComponentIndex"
+import {Banner, Experience,Footer,Header,Sidebar} from "@/app/compontents/ComponentIndex"
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
     <Sidebar/>
     <main className='snap-y snap-mandatory md:mx-auto lg:mx-[15rem] py-16 px-7'>
       <section id="Top">
+        <Banner/>
         <Header/>
       </section>
       <Experience/> 
