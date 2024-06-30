497686cd5f093351106a5dda3d4aa0b29ea71394
louispawaon <lpawaon@gmail.com>
2024-06-30 21:17:52 +0800

    refactor: properly split components to each tsx file

--- src/app/compontents/ComponentIndex.tsx
+++ src/app/compontents/ComponentIndex.tsx
@@ -1,7 +1,8 @@
-export {default as Creations} from './Experience/Creations';
+export {default as Creations} from './Creations/Creations';
 export {default as Experience} from './Experience/Experience';
-export {default as Card} from './Experience/Card';
+export {default as Card} from './Card/Card';
 export {default as Footer} from './Footer/Footer';
 export {default as Header} from './Header/Header';
 export {default as Sidebar} from './SideBar/Sidebar';
-export {default as Technologies} from './Experience/Technologies';
+export {default as TechAndTools} from './Technologies/TechAndTools';
+export {default as ContentSection} from './Section/ContentSection';
---
+++ src/app/compontents/Technologies/TechAndTools.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { technologies } from '@/data/Technologies'
+
+const TechAndTools = () => {
+  return (
+    <div className="space-y-3 mb-10 md:mb-5 xl:mb-10">
+      <h1 className="text-2xl font-semibold">Technologies and Tools</h1>
+      <div className="grid grid-cols-1 gap-4">
+        {technologies.map((tech, index) => (
+          <div key={index}>
+            <span
+              className={`font-semibold text-sm p-1 bg-[${tech.color}] text-[${tech.textColor}]`}
+            >
+              {tech.title}
+            </span>
+            <div className="pt-3 flex flex-wrap justify-start">
+              {tech.items.map((item, index) => (
+                <item.icon
+                  key={index}
+                  className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] lg:w-[50px] lg:h-[50px] mx-2"
+                  title={item.title}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TechAndTools;
