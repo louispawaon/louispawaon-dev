a2df9d3fa4176a5c3f5473c4c957a37be275ed82
louispawaon <lpawaon@gmail.com>
2024-07-07 20:46:55 +0800

    refactor: change project type and add new projects

--- lib/types/ProjectType.ts
+++ lib/types/ProjectType.ts
@@ -1,16 +1,11 @@
-import type { StaticImageData } from "next/image";
-
 export type ProjectType =  {
-    image: StaticImageData;
-    alt: string;
     title: string;
-    date: string;
     roles: {
       name: string;
       color: string;
       textColor: string;
     }[];
     description: string[];
+    technologies: string[];
     link: string;
-    linkText: string;
   }
--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,38 +1,53 @@
 import { ProjectType } from "lib/types/ProjectType";
-import ConstiWebsite from '../../public/ProjectAssets/ConstiWebsite.png'
-import GSTA from '../../public/ProjectAssets/GSTAEnrollmentSystem.jpg'
   
   export const projects: ProjectType[] = [
     {
-      image: ConstiWebsite,
-      alt: "ConstiWeb",
-      title: "SAMAHAN Consti Natin 'To Voting Website",
-      date: "Jan 2023 - Feb 2023",
+
+      title: "SmartCampus",
       roles: [
         { name: "Backend Developer", color: "bg-[rgba(211,78,128,.1)]", textColor: "#d34e80" },
-        { name: "Database Administrator", color: "bg-[rgba(9,105,218,.1)]", textColor: "#0969da" },
+        { name: "Dataabse Administrator", color: "bg-[rgba(9,105,218,.1)]", textColor: "#0969da" },
       ],
       description: [
-        "The Voting Website for the ratification of the proposed amendment of the changes in the 2020 SAMAHAN Constitution. I Developed the Backend Framework and Database Management using JavaScript, Firebase and Firestore.",
-        "With about 3000 successful voting entries, the changes of the 2020 SAMAHAN Constitution has been ratified.",
+        "One-Stop Shop Facility Reservation Mobile Application for Students, Teachers and School Staff"
       ],
-      link: "https://visionary-peony-079a58.netlify.app/",
-      linkText: "Visit Website",
+      technologies: ["NestJS", "TypeScript", "Supabase", "PostgreSQL", "Prisma"],
+      link: "https://github.com/louispawaon/smartcampus-backend",
     },
     {
-      image: GSTA,
-      alt: "GSTASystem",
-      title: "GSTA Enrollment System",
-      date: "Jun 2022 - Oct 2022",
+
+      title: "heic-to-jpg",
       roles: [
-        { name: "Backend Developer", color: "bg-[rgba(211,78,128,.1)]", textColor: "#d34e80" },
-        { name: "Database Administrator", color: "bg-[rgba(9,105,218,.1)]", textColor: "#0969da" },
+        { name: "Developer", color: "bg-[rgba(218,122,9,.1)]", textColor: "#da7a09" },
       ],
       description: [
-        "An Enrollment System for a Security Training Center which is part of our Software Engineering 1&2 Courseware Overall Project. I Developed the Backend Framework and Database Management using TypeScript, Node, Express and PostgreSQL. I also used Prisma, an ORM Framework to bridge between the database and the api endpoints to perform complex CRUD Operations.",
-        "It was evaluated as a successful project with a final average grade of A (92%-100%).",
+        "A Convenient Way To Convert Bulk .heic Files to .jpg Files"
       ],
-      link: "https://github.com/louispawaon/goncar-enrollment-system-backend",
-      linkText: "Visit Repository",
+      technologies: ["Python"],
+      link: "https://github.com/louispawaon/heic-to-jpg",
+    },
+    {
+
+      title: "PulseCheck",
+      roles: [
+        { name: "Developer", color: "bg-[rgba(26,127,55,.1)]", textColor: "#1a7f37" },
+      ],
+      description: [
+        "Your go-to destination for effortlessly checking the online pulse of Ateneo de Davao University (AdDU)."
+      ],
+      technologies: ["Next.js", "React", "TypeScript", "TailwindCSS", "shadcn/ui"],
+      link: "https://pulsecheck.vercel.app/",
+    },
+    {
+
+      title: "Sikwati AI",
+      roles: [
+        { name: "Backend Developer", color: "bg-[rgba(211,78,128,.1)]", textColor: "#d34e80" },
+      ],
+      description: [
+        "Mobile Application for detecting local cacao pod diseases with the EfficientNetB0 CNN Model"
+      ],
+      technologies: ["Python", "Flask"],
+      link: "https://github.com/louispawaon/sikwati-ai-backend",
     },
   ];
