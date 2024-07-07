33b1fd3c347d1cf23664c88d6d5f151ac59a391e
louispawaon <lpawaon@gmail.com>
2024-07-07 21:01:57 +0800

    feat: opengraph metadata image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Louis Miguel Pawaon',
   description: 'Portfolio Website of Louis Miguel Pawaon',
+  metadataBase: new URL('https://louispawaon.vercel.app/')
 }
 
 export default function RootLayout({
